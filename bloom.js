44e3b4ec5d8f47d33a0a73feeb828edda163a19b
itony215 <itony215@gmail.com>
2020-10-12 16:49:37 +0800

    bloom

---
+++ bloom.js
@@ -0,0 +1,30 @@
+function makeid(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const fetch = require('node-fetch')
+
+let garbage = makeid(45000000)
+let url = 'http://bld.2tvnow.com/annotationService/testSave.json'
+for (i = 0; i <= 100; i++) {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "user_name": "jl-du",
+      "file_name": "TW00100007",
+      "study": garbage,
+      "annotation": garbage
+    })
+  }).then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.log('error:', err))
+}
